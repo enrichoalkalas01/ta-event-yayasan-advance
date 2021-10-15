6bd437d9c94b9d5447e35a7e2b971307fd15be21
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2021-10-15 21:00:44 +0700

    add files event

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -61,7 +61,7 @@ AppAsset::register($this);
 </header>
 
 <main role="main" class="flex-shrink-0">
-    <div class="container">
+    <div class="container-fluid" style="padding: 0px;">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
