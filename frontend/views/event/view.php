ba5890ab809a37e191b6e8cb55d853f9a0e6ca21
featriadi <aditya.fitriadi99@gmail.com>
2021-10-16 08:15:33 +0700

    event detail

--- frontend/views/event/view.php
+++ frontend/views/event/view.php
@@ -6,39 +6,66 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Event */
 
-$this->title = $model->id;
+$this->title = $model->event_name;
 $this->params['breadcrumbs'][] = ['label' => 'Events', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
+
 <div class="event-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<section id="app" class="container-fluid">
+    
+    <!-- Banner Homepages -->
+    <div class="row banner-box">
+        <div class="col-md-12 content">
+            <div class="image-box" style="height: 350px; background-image: url('https://cdn1-production-images-kly.akamaized.net/CTp92DhnXYO7tlLuIYR95eA416A=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3343581/original/095226300_1610083522-plastic-building-blocks-background-3d-render_68747-136.jpg');">
+                <div class="backdrop">
+                    <div class="box-text"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <!-- Section 1 -->
+    <div class="row event-box" id="events-detail">
+            <div class="col-lg-8 col-md-7 col-sm-12 wrapper-left">
+                <div class="row wrapper-box">
+                    <div class="col-md-12 title-box">
+                        <h1><?php echo $model->event_name ?></h1>
+                    </div>
+                    <div class="col-md-12 description-box">
+                        <?php echo $model->description ?>
+                    </div>
+                </div>
+            </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'event_name',
-            'date_start',
-            'date_end',
-            'description:ntext',
-            'image',
-            'fee',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+            <div class="col-lg-4 col-md-5 col-sm-12 wrapper-right">
+                <div class="row wrapper-box">
+                    <div class="col-md-12 card">
+                        <div class="wrapper-top btn-primary">
+                            <h5>Buy Tickets Here..</h5>
+                        </div>
+                        <!-- <div class="wrapper-middle">
+                            <span class="bold">Date Register : </span>
+                            <span class="thin">1 November 2021 - 1 December 2021</span>
+                        </div> -->
+                        <div class="wrapper-middle">
+                            <span class="bold">Date Event : </span>
+                            <span class="thin"><?php echo $model->date_start ?> - <?php echo $model->date_end ?></span>
+                        </div>
+                        <div class="wrapper-middle">
+                            <span class="bold">Ticket Price : </span>
+                            <span>Rp.</span>
+                            <span><?php echo $model->fee ?></span>
+                        </div>
+                        <form class="ticket-box" action="" method="post">
+                            <input name="ticket-data" value="1" id="ticket0-data" hidden />
+                            <button type="submit" class="btn btn-primary">Join Event</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
 
-</div>
