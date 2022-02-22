86e84ed038e007b8fedda385122521f7224eee51
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-02-23 00:23:18 +0700

    add changes frontend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,6 +21,10 @@ AppAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Roboto+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,15 +6,58 @@
 $this->title = 'My Yii Application';
 ?>
 
-<section id="app" class="container-fluid">
+<section id="app2" class="container-fluid">
     <!-- Banner Homepages -->
     <div class="row banner-box">
         <div class="col-md-12 content">
-            <div class="image-box" style="background-image: url('https://cdn1-production-images-kly.akamaized.net/CTp92DhnXYO7tlLuIYR95eA416A=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3343581/original/095226300_1610083522-plastic-building-blocks-background-3d-render_68747-136.jpg');">
+            <div class="image-box" style="background-image: url('images/banner-thumbnail.jpg');">
                 <div class="backdrop">
                     <div class="box-text">
-                        <h1>Title Banner Here</h1>
-                        <p>Description here</p>
+                        <h1 class="roboto m-0" style="font-weight: 1000;">We Help, Because We Care</h1>
+                        <p class="nunito">We love to help people who need a help</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mission-box">
+        <div class="col-12 title-box text-center mb-4">
+            <h2 class="roboto font-bold">Visi & Misi</h2>
+        </div>
+        <div class="col-12 content-box">
+            <div class="row wrapper-content-box">
+                <div class="col-12 col-lg-4 card-content">
+                    <div class="card" style="width: 18rem;">
+                        <div class="icon-box text-center">
+                            <i class="fa-solid fa-graduation-cap"></i>
+                        </div>
+                        <div class="card-body text-center">
+                            <h5 class="card-title roboto">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 card-content">
+                    <div class="card" style="width: 18rem;">
+                        <div class="icon-box text-center">
+                            <i class="fa-solid fa-graduation-cap"></i>
+                        </div>
+                        <div class="card-body text-center">
+                            <h5 class="card-title roboto">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 card-content">
+                    <div class="card" style="width: 18rem;">
+                        <div class="icon-box text-center">
+                            <i class="fa-solid fa-graduation-cap"></i>
+                        </div>
+                        <div class="card-body text-center">
+                            <h5 class="card-title roboto">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -22,7 +65,7 @@ $this->title = 'My Yii Application';
     </div>
 
     <!-- Section 1 -->
-    <div class="row section-1">
+    <!-- <div class="row section-1">
         <div class="col-md-12 title-box text-center" style="padding: 20px 0;">
             <h2>Event Kami</h2>
         </div>
@@ -35,10 +78,10 @@ $this->title = 'My Yii Application';
                 'class' => 'col-sm-12 col-md-6 col-lg-3'
             ]
         ]) ?>
-    </div>
+    </div> -->
 
     <!-- Section 2 -->
-    <div class="row section-2">
+    <!-- <div class="row section-2">
         <div class="col-md-12 section-box">
             <div class="row wrapper-section">
                 <div class="col-md-6 left-side text-box">
@@ -61,35 +104,19 @@ $this->title = 'My Yii Application';
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <!-- Section 2 -->
-    <div class="row section-2">
-        <div class="col-md-12 section-box">
-            <div class="row wrapper-section">
-                <div class="col-md-6 left-side images-box">
-                    <div>
-                        <img class="image" src="images/fe2.jpg" alt="gambar">
-                    </div>    
-                </div>
-                <div class="col-md-6 right-side text-box">
-                    <div class="wrapper-text">
-                        <!-- <h4>Title Here</h4> -->
-                        <p>
-                            Berbagai event Yayasan Sosial Yatim Piatu dan Duafa Anggi Foundation antara lain berbentuk lomba, seminar, pentas seni, festival, dan lain-lain. Penyelenggara berbagai event tersebut ada berbagai macam. Antara lain adalah dari intern yayasan, instansi swasta dan pemerintah seperti Kementerian Sosial Republik Indonesia, Kementerian Pendidikan dan Kebudayaan Republik Indonesia. 
-                            Pengurus, anak-anak asuh, dan donatur akan memperoleh berbagai hal yang berguna dengan mengikuti event di luar yayasan. 
-                            Misalnya dengan mengikuti seminar tentang teknologi, anak-anak asuh akan memperoleh materi-materi tentang teknologi yang belum tentu diajarkan di yayasan. Selain itu dengan mengikuti event di luar yayasan, anak-anak asuh akan mendapatkan relasi yang lebih luas. 
-                        </p>
-                        <p>
-                            Anggi Foundation pernah menjalin kerjasama dengan berbagai organisasi di tingkat regional (Asia Tenggara) dan Internasional. Organisasi tersebut antara lain: Youth Coordination Center International (YCCI); President Office for Youth Affairs (Filipina); TYAP (Thai Youth AIDS Prevention) di Thailand; LOMEF (Lorraine Robinson and Maeve Coughlan Education Fund) yang berada di Amerika Serikat; 
-                            ARF (Asian Resource Foundation); AMAN (Asian Muslim Action Network); Singapore Young Enterpreneus Network; APEC Youth Forum; dan Yayasan Penyayang di Malaysia. 
-                        </p>
-                        <p>
-                            Anggi Foundation dengan motto Lets Care and Share. Tetap aktif dalam menjalankan program program sosialnya dan kegiatan event dengan baik.
-                        </p>
-                    </div>
+    <div class="row profile-box">
+        <div class="col-12 col-lg-6 left-content">
+            <div class="row wrapper-content">
+                <div class="col-12">
+                    
                 </div>
             </div>
+        </div>
+        <div class="col-12 col-lg-6 right-content">
+
         </div>
     </div>
 </section>
