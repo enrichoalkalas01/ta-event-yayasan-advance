a27441659ef0534586e5575e23417f895af87f7e
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2022-02-23 01:43:51 +0700

    add change style homepages

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -33,7 +33,7 @@ $this->title = 'My Yii Application';
                             <i class="fa-solid fa-graduation-cap"></i>
                         </div>
                         <div class="card-body text-center">
-                            <h5 class="card-title roboto">Card title</h5>
+                            <h5 class="card-title roboto">Education</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
@@ -41,10 +41,10 @@ $this->title = 'My Yii Application';
                 <div class="col-12 col-lg-4 card-content">
                     <div class="card" style="width: 18rem;">
                         <div class="icon-box text-center">
-                            <i class="fa-solid fa-graduation-cap"></i>
+                            <i class="fa-solid fa-people-carry-box"></i>
                         </div>
                         <div class="card-body text-center">
-                            <h5 class="card-title roboto">Card title</h5>
+                            <h5 class="card-title roboto">Social</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
@@ -52,10 +52,10 @@ $this->title = 'My Yii Application';
                 <div class="col-12 col-lg-4 card-content">
                     <div class="card" style="width: 18rem;">
                         <div class="icon-box text-center">
-                            <i class="fa-solid fa-graduation-cap"></i>
+                            <i class="fa-solid fa-hand-holding-heart"></i>
                         </div>
                         <div class="card-body text-center">
-                            <h5 class="card-title roboto">Card title</h5>
+                            <h5 class="card-title roboto">Care</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
@@ -64,80 +64,122 @@ $this->title = 'My Yii Application';
         </div>
     </div>
 
-    <!-- Section 1 -->
-    <!-- <div class="row section-1">
-        <div class="col-md-12 title-box text-center" style="padding: 20px 0;">
-            <h2>Event Kami</h2>
-        </div>
-
-        <?php echo \yii\widgets\ListView::widget([
-            'dataProvider' => $dataProvider,
-            'layout' => '{summary}<div class="row">{items}</div>{pager}',
-            'itemView' => '_event_item',
-            'itemOptions' => [
-                'class' => 'col-sm-12 col-md-6 col-lg-3'
-            ]
-        ]) ?>
-    </div> -->
-
-    <!-- Section 2 -->
-    <!-- <div class="row section-2">
-        <div class="col-md-12 section-box">
-            <div class="row wrapper-section">
-                <div class="col-md-6 left-side text-box">
-                    <div class="wrapper-text">
-                        <h4>Profil Yayasan Sosial Yatim Piatu & Duafa Anggi Foundation</h4>
-                        <p>
-                            Anggi Foundation merupakan organisasi yang bergerak dibidang pendidikan. Anggi Foundation berdiri pada tanggal 13 Mei 1999, sesaat setelah wafatnya putri tercinta, Anggi Ratri Anggorokasih dalam usia 20 tahun. 
-                            Almarhumah Anggi meninggal karena kecelakaan saat akan berangkat kuliah di Universitas Indonesia, Depok. Kepergian Anggi ke haribaan Illahi, menggugah kami, orang tua, sanak keluarga dan sahabat-sahabatnya, terpanggil untuk meneruskan cita-cita luhurnya. 
-                            Gadis belia yang menyukai tugas-tugas sosial ini, selalu terpacu untuk membantu anak-anak pintar yang tidak mampu. Keinginan itu telah dirintisnya ketika setiap hari minggu, ia meluangkan waktu di pagi hari, datang ke pesantren Nurul Huda untuk mengajar Bahasa Inggris. 
-                        </p>
-                        <p>
-                            Dengan harapan ingin meneruskan cita-cita almarhumah, keluarga sepakat untuk mendirikan yayasan sosial bernama Anggi Foundation. Yayasan Sosial Yatim Piatu dan Duafa Anggi Foundation merupakan lembaga sosial non panti yang telah berbadan hukum dengan Akta Notaris Ibu Masnah Sari, S.H. No. 125 Tahun 1999 dan mempunyai Visi dan Misi yaitu akses pendidikan yang meyeluruh dengan memberikan bantuan di bidang pendidikan kepada masyarakat yang kurang mampu. 
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-6 right-side images-box">
-                    <div>
-                        <img class="image" src="images/fe1.jpg" alt="gambar">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
-    <!-- Section 2 -->
     <div class="row profile-box">
         <div class="col-12 col-lg-6 left-content">
             <div class="row wrapper-content">
-                <div class="col-12">
-                    
+                <div class="col-12 top-text">
+                    <span class="font-bold" style="color: #46a4f7;">About</span>
+                </div>
+                <div class="col-12 title-box">
+                    <h4>Profil Yayasan Sosial Yatim Piatu & Duafa Anggi Foundation</h4>
+                </div>
+                <div class="col-12 descrpition-box">
+                    <p>
+                        Anggi Foundation merupakan organisasi yang bergerak dibidang pendidikan. Anggi Foundation berdiri pada tanggal 13 Mei 1999, sesaat setelah wafatnya putri tercinta, Anggi Ratri Anggorokasih dalam usia 20 tahun. 
+                        Almarhumah Anggi meninggal karena kecelakaan saat akan berangkat kuliah di Universitas Indonesia, Depok. 
+                    </p>
+                    <p>
+                        Kepergian Anggi ke haribaan Illahi, menggugah kami, orang tua, sanak keluarga dan sahabat-sahabatnya, terpanggil untuk meneruskan cita-cita luhurnya. 
+                        Gadis belia yang menyukai tugas-tugas sosial ini, selalu terpacu untuk membantu anak-anak pintar yang tidak mampu. Keinginan itu telah dirintisnya ketika setiap hari minggu, ia meluangkan waktu di pagi hari, datang ke pesantren Nurul Huda untuk mengajar Bahasa Inggris.
+                    </p>
+                    <p>
+                        Dengan harapan ingin meneruskan cita-cita almarhumah, keluarga sepakat untuk mendirikan yayasan sosial bernama Anggi Foundation. Yayasan Sosial Yatim Piatu dan Duafa Anggi Foundation
+                        merupakan lembaga sosial non panti yang telah berbadan hukum dengan Akta Notaris Ibu Masnah Sari, S.H. No. 125 Tahun 1999
+                        dan mempunyai Visi dan Misi yaitu akses pendidikan yang meyeluruh dengan memberikan bantuan di bidang pendidikan kepada masyarakat yang kurang mampu.
+                    </p>
                 </div>
             </div>
         </div>
         <div class="col-12 col-lg-6 right-content">
+            <div class="row wrapper-content">
+                <div class="col-12 wrapper-images">
+                    <div class="images" style="background-image: url('images/fe1.jpg')"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-12 bottom-content">
+            <div class="row wrapper-content">
+                <div class="col-12 descrpition-box">
+                    <p>
+                        Berbagai event Yayasan Sosial Yatim Piatu dan Duafa Anggi Foundation antara lain berbentuk lomba, seminar, pentas seni, festival, dan lain-lain.
+                        Penyelenggara berbagai event tersebut ada berbagai macam. Antara lain adalah dari intern yayasan, instansi swasta dan pemerintah seperti
+                        Kementerian Sosial Republik Indonesia, Kementerian Pendidikan dan Kebudayaan Republik Indonesia.
+                    </p>
+                    <div class="wrapper-images">
+                        <div class="images" style="background-image: url('images/fe2.jpg')"></div>
+                    </div>
+                    <p>
+                        Pengurus, anak-anak asuh, dan donatur akan memperoleh
+                        berbagai hal yang berguna dengan mengikuti event di luar yayasan. Misalnya dengan mengikuti seminar tentang teknologi, anak-anak asuh akan memperoleh
+                        materi-materi tentang teknologi yang belum tentu diajarkan di yayasan. Selain itu dengan mengikuti event di luar yayasan, anak-anak asuh akan mendapatkan
+                        relasi yang lebih luas. 
+                    </p>
 
+                    <p>
+                        Anggi Foundation pernah menjalin kerjasama dengan berbagai organisasi di tingkat regional (Asia Tenggara) dan Internasional.
+                        <b> Organisasi tersebut antara lain: Youth Coordination Center International (YCCI); </b>
+                        <b> President Office for Youth Affairs (Filipina); </b>
+                        <b> TYAP (Thai Youth AIDS Prevention) di Thailand; </b>
+                        <b> LOMEF (Lorraine Robinson and Maeve Coughlan Education Fund) yang berada di Amerika Serikat; </b>
+                        <b> ARF (Asian Resource Foundation); </b>
+                        <b> AMAN (Asian Muslim Action Network); </b>
+                        <b> Singapore Young Enterpreneus Network; </b>
+                        <b> APEC Youth Forum; </b>
+                        dan <b> Yayasan Penyayang di Malaysia. </b>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section Event -->
+    <div class="row section-1">
+        <div class="col-md-12 title-box text-center" style="padding: 10px 0 0px;">
+            <h4 class="roboto font-bold">Event Kami</h4>
+        </div>
+
+        <div class="wrapper-event" style="padding: 0px 5% 25px;">
+            <?php echo \yii\widgets\ListView::widget([
+                'dataProvider' => $dataProvider,
+                'layout' => '{summary}<div class="row">{items}</div>{pager}',
+                'itemView' => '_event_item',
+                'itemOptions' => [
+                    'class' => 'col-sm-12 col-md-6 col-lg-3'
+                ]
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="row profile-box">
+        <div class="col-12 col-lg-12 bottom-content">
+            <div class="row wrapper-content">
+                <div class="col-12 descrpition-box">
+                    <h4 class="roboto">Anggi Foundation pernah menjalin kerjasama dengan berbagai organisasi di tingkat regional (Asia Tenggara) dan Internasional.</h4>
+                    <p>
+                        <b> Organisasi tersebut antara lain: Youth Coordination Center International (YCCI); </b>
+                        <b> President Office for Youth Affairs (Filipina); </b>
+                        <b> TYAP (Thai Youth AIDS Prevention) di Thailand; </b>
+                        <b> LOMEF (Lorraine Robinson and Maeve Coughlan Education Fund) yang berada di Amerika Serikat; </b>
+                        <b> ARF (Asian Resource Foundation); </b>
+                        <b> AMAN (Asian Muslim Action Network); </b>
+                        <b> Singapore Young Enterpreneus Network; </b>
+                        <b> APEC Youth Forum; </b>
+                        dan <b> Yayasan Penyayang di Malaysia. </b>
+                    </p>
+
+                    <div class="wrapper-images">
+                        <div class="images" style="background-image: url('images/fe3.jpg')"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row quote-box">
+        <div class="col-12 content">
+            <span class="quote-t left">"</span>
+            <span class="quote-c">Anggi Foundation dengan motto Lets Care and Share. Tetap aktif dalam menjalankan program program sosialnya dan kegiatan event dengan baik.</span>
+            <span class="quote-t right">"</span>
         </div>
     </div>
 </section>
-
-<!-- <script type="text/javascript">
-    let ProductSectionOne = document.querySelector('.section-1 .product-box .wrapper-product')
-    for ( let i = 0; i < 4; i++ ) {
-        ProductSectionOne.innerHTML += `
-            <div class="col-sm-12 col-md-6 col-lg-3 products products-${ i }">
-                <div class="wrapper-product card">
-                    <a href="">
-                        <div class="image-box">
-                            <div class="images" style="background-image: url('https://cdn1-production-images-kly.akamaized.net/CTp92DhnXYO7tlLuIYR95eA416A=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3343581/original/095226300_1610083522-plastic-building-blocks-background-3d-render_68747-136.jpg');"></div>
-                        </div>
-                        <div class="text-box">
-                            <h4>Title product ${ i + 1 }</h4>
-                            <p>description product</p>
-                        </div>    
-                    </a>
-                </div>
-            </div>
-        `
-    }
-</script> -->
